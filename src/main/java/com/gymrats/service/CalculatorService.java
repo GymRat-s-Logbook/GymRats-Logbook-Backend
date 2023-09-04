c2caba45936924371f961252aab0cb86b5e995ef
Julia0321 <241050@edu.p.lodz.pl>
2023-09-04 22:50:26 +0200

    AsNotLoggedCountBmiFixed

--- src/main/java/com/gymrats/service/CalculatorService.java
+++ src/main/java/com/gymrats/service/CalculatorService.java
@@ -7,12 +7,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class CalculatorService {
 
-
     public BmiNotLoggedResponse calculateBmi(BmiNotLoggedRequest requestBmi) {
         float weight = requestBmi.weight();
         float height = requestBmi.height();
         float bmi = weight / (height * height);
-        BmiNotLoggedResponse bmiResponse = new BmiNotLoggedResponse(bmi);
-        return bmiResponse;
+        return new BmiNotLoggedResponse(bmi);
     }
 }
