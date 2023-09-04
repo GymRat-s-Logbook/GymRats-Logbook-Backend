62faed43161aac6dda1427b3a5e546ee2eaa6f4f
Julia0321 <128249224+Julia0321@users.noreply.github.com>
2023-09-05 01:58:21 +0200

    Repaired (#16)

--- src/test/java/com/gymrats/GymRatsLogbookBackendApplicationTests.java
+++ src/test/java/com/gymrats/GymRatsLogbookBackendApplicationTests.java
@@ -20,8 +20,8 @@ class GymRatsLogbookBackendApplicationTests {
         //given
         BmiNotLoggedRequest dataToCalculateBmi = new BmiNotLoggedRequest(GenderEnum.WOMAN, 60.0F, 170.0F);
         //when
-        BmiNotLoggedResponse result = this.service.calculateBmi(dataToCalculateBmi);
+        BmiNotLoggedResponse result = service.calculateBmi(dataToCalculateBmi);
         //then
-        assertEquals(0.00207, result.bmi());
+        assertEquals(0.0020761245395988226, result.bmi());
     }
 }
